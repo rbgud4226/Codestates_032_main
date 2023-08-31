ad9d94923089b371e5bb2aa859acbc104635e93e
udaeng8286 <udaenglvup@gmail.com>
2023-08-31 12:16:46 +0900

    Design : 공통 스타일 지정 -Roboto 글꼴 -전체 레이아웃 설정 Isuue : #16

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
         <GlobalStyles />
+        <Main>
+          <Container>
+            <Header />
+            <Wrapper>배고파</Wrapper>
+          </Container>
+        </Main>
       </Router>
     </>
   );
@@ -19,5 +25,31 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     text-decoration: none;
+    font-family: 'Roboto';
   }
+`;
+const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+  margin: 0px;
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 393px;
+  min-width: 320px;
+  min-height: 100vh;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 8px 36px;
+`;
+
+const Header = styled.div`
+  min-height: 70px;
+  min-width: 320px;
+  background-color: aqua;
+`;
+
+const Wrapper = styled.div`
+  min-height: 100%;
+  margin: 28px;
+  background-color: pink;
 `;
