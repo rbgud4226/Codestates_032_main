b601b531c1e2e78cf57620578247889c7d885417
sebfe45kimck <kcg7357@gmail.com>
2023-09-06 18:20:13 +0900

    변경사항저장

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,26 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { BrowserRouter as Router } from "react-router-dom";
-=======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
-import { BrowserRouter as Router } from "react-router-dom";
->>>>>>> a06214b (fix:nav를 제외한 헤더및 페이지 삭제)
+import Mypage from "./page/Mypage";
+import Layout from "./Layout";
 import Nav from "./component/nav/Nav";
-import LoginPage from "./page/LoginPage";
-<<<<<<< HEAD
-import MemberAgreePage from "./page/memberAgreePage";
-import SignUpPage from "./page/SignUpPage";
-import MainPage from "./page/MainPage";
 
->>>>>>> 27a48d4 (feat:Nav 뷰 구현)
-=======
->>>>>>> a06214b (fix:nav를 제외한 헤더및 페이지 삭제)
 function App() {
   return (
     <>
       <Router>
         <GlobalStyles />
-        <Main>
+        <Body>
           <Container>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <Wrapper></Wrapper>
-=======
             <Header />
-=======
->>>>>>> a06214b (fix:nav를 제외한 헤더및 페이지 삭제)
-            <Wrapper>
-              <LoginPage />
-            </Wrapper>
+            <Layout>
+              <Routes>
+                <Route path="/members" element={<Mypage />} />
+              </Routes>
+            </Layout>
             <Nav />
->>>>>>> 27a48d4 (feat:Nav 뷰 구현)
           </Container>
-        </Main>
+        </Body>
       </Router>
     </>
   );
@@ -54,21 +36,21 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Roboto';
   }
 `;
-const Main = styled.main`
+const Body = styled.body`
   width: 100vw;
-  height: 100vh;
   margin: 0px;
   display: flex;
   justify-content: center;
 `;
 const Container = styled.div`
-  width: 393px;
+  width: 500px;
   min-width: 320px;
   min-height: 100vh;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 8px 36px;
 `;
 
-const Wrapper = styled.div`
-  min-height: 100%;
-  margin: 28px;
+const Header = styled.div`
+  min-height: 70px;
+  min-width: 320px;
+  background-color: aqua;
 `;
