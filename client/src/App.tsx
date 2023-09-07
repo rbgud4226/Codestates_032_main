5ef5d83b30c033061d7f8cfa09f3bb41010dc3f2
Dohyun1225 <dohyun122598@gmail.com>
2023-09-07 12:03:05 +0900

    feat:#8 기능 및 css 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-import { BrowserRouter as Router } from "react-router-dom";
-import MainForm from "./component/MainForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mypage from "./page/Mypage";
+import Layout from "./Layout";
+import Header from "./component/header/Header";
+import Nav from "./component/nav/Nav";
+
 function App() {
   return (
     <>
       <Router>
         <GlobalStyles />
-        <Main>
+        <Body>
           <Container>
             <Header />
-            <MainForm propertyName="example" />
+            <Layout>
+              <Routes>
+                <Route path="/members" element={<Mypage />} />
+              </Routes>
+            </Layout>
+            <Nav />
           </Container>
-        </Main>
+        </Body>
       </Router>
     </>
   );
@@ -26,12 +35,10 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     text-decoration: none;
     font-family: 'Roboto';
-    background-color: while;
   }
 `;
-const Main = styled.main`
+const Body = styled.body`
   width: 100vw;
-  height: 100%;
   margin: 0px;
   display: flex;
   justify-content: center;
@@ -41,16 +48,4 @@ const Container = styled.div`
   min-width: 320px;
   min-height: 100vh;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 8px 36px;
-`;
-
-const Header = styled.div`
-  min-height: 70px;
-  min-width: 320px;
-  background-color: aqua;
-`;
-
-const Wrapper = styled.div`
-  min-height: 100%;
-  margin: 28px;
-  background-color: pink;
 `;
