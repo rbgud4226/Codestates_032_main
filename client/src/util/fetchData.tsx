d2192bc42fff4fcc731c1027d033089f1496792b
sebfe45kimck <kcg7357@gmail.com>
2023-09-10 16:52:26 +0900

    nav수정 및 추가라이브러리설치

--- client/src/util/fetchData.tsx
+++ client/src/util/fetchData.tsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 //NGrok 갱신마다 이 페이지의 주소만 바꾸면됨.
 export async function fetchData({ email, password }, setErrorMsg) {
+  const api = process.env.REACT_APP_DB_HOST;
+
   try {
     const data = {
       email: email,
       password: password,
     };
-    const res = await axios.post(
-      "https://7fdc-218-155-160-190.ngrok-free.app/members/login",
-      data,
-    );
+    const res = await axios.post(`${api}/members/login`, data);
     console.log(res);
     const userData = {
       nickName: res.data.nickName,
