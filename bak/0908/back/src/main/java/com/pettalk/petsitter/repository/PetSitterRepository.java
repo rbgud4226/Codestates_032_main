bb379e3321b7914615110e1095915c30db277856
j00r6 <jindory8@gmail.com>
2023-09-11 19:15:11 +0900

    백업 파일 경로 삭제

--- bak/0908/back/src/main/java/com/pettalk/petsitter/repository/PetSitterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pettalk.petsitter.repository;
-
-import com.pettalk.petsitter.entity.PetSitter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetSitterRepository extends JpaRepository<PetSitter, Long> {
-}
