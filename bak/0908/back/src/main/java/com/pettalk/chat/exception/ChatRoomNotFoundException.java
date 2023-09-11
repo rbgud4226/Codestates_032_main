80aa6163f537b20ae4e1ad58cc3e2694e1733a48
j00r6 <130388654+j00r6@users.noreply.github.com>
2023-09-11 12:24:12 +0900

    Delete bak/0908/back directory
    백업파일 삭제

--- bak/0908/back/src/main/java/com/pettalk/chat/exception/ChatRoomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.pettalk.chat.exception;
-
-public class ChatRoomNotFoundException extends RuntimeException{
-    public ChatRoomNotFoundException(String message){
-        super(message);
-    }
-}
