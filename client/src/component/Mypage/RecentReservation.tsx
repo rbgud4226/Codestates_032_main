154fd8d175613c41a071a78713672a5277d422b0
udaeng8286 <udaenglvup@gmail.com>
2023-09-19 12:34:16 +0900

    feat : 수정

--- client/src/component/Mypage/RecentReservation.tsx
+++ client/src/component/Mypage/RecentReservation.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import ProfileImg from "./ProfileImg";
 import axios from "axios";
 
 const RecentReservation = () => {
